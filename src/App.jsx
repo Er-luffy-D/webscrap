a995a8c0b5ccff19e5cdcad7ad7a6a7e6270683d
Piyush <02jan06@gmail.com>
2024-10-10 15:23:59 +0530

    Revert "Merge branch 'main' of https://github.com/Er-luffy-D/webscrap"
    This reverts commit efc9c48b864c9236c4b63dfc4360616b8e81b6bd, reversing
    changes made to d51cefd799d3e0a4677ae742e811b0f162127174.

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
   };
 
   return (
-    <div className="App ">
+    <div className="App h-screen">
       <h1 className="ml-2">
         {data.length == 0 ? "Data is Fetching.." : "Data Fetched successfully"}
       </h1>
